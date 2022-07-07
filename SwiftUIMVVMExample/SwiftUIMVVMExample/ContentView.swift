fa88b2da741a2cb8c1b88aca729a7a7f4944b7c5
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-07-06 21:21:28 -0300

    Initial Scafolding

---
+++ SwiftUIMVVMExample/SwiftUIMVVMExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMVVMExample
+//
+//  Created by Ezequiel Rasgido on 06/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMVVMExample/SwiftUIMVVMExample/SwiftUIMVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMExampleApp.swift
+//  SwiftUIMVVMExample
+//
+//  Created by Ezequiel Rasgido on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
